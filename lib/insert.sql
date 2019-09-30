5002aac696a8b3bee2cd073965acfcae85936baf
Andrew Silva <andrewsilva9@gmail.com>
2019-09-30 01:37:39 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,13 +1,13 @@
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, "Project 1", "Category 1", 100, "9/29/19", "9/30/19");
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (2, "Project 2", "Category 2", 200, "10/1/19", "10/5/19");
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (3, "Project 3", "Category 3", 300, "10/1/19", "10/10/19");
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (4, "Project 4", "Category 4", 400, "9/30/19", "10/6/19");
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (5, "Project 5", "Category 5", 500, "9/30/19", "10/1/19");
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (6, "Project 6", "Category 6", 600, "10/1/19", "10/6/19");
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (7, "Project 7", "Category 7", 700, "9/29/19", "9/30/19");
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (8, "Project 8", "Category 8", 800, "10/1/19", "10/4/19");
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (9, "Project 9", "Category 9", 900, "9/30/19", "10/3/19");
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (10, "Project 10", "Category 10", 1000, "10/1/19", "10/8/19");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VAL("Project 1", "Category 1", 100, "9/29/19", "9/30/19");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Project 2", "Category 2", 200, "10/1/19", "10/5/19");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Project 3", "Category 3", 300, "10/1/19", "10/10/19");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Project 4", "Category 4", 400, "9/30/19", "10/6/19");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Project 5", "Category 5", 500, "9/30/19", "10/1/19");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Project 6", "Category 6", 600, "10/1/19", "10/6/19");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Project 7", "Category 7", 700, "9/29/19", "9/30/19");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Project 8", "Category 8", 800, "10/1/19", "10/4/19");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Project 9", "Category 9", 900, "9/30/19", "10/3/19");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Project 10", "Category 10", 1000, "10/1/19", "10/8/19");
 
 INSERT INTO users (id, name, age) VALUES (1, "Andrew", 30);
 INSERT INTO users (id, name, age) VALUES (2, "Jessica", 29);
